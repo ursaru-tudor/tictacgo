202daa08e698e9578dd4eecb359c463bf0102c68
ursaru-tudor <tudor.ursaru07@gmail.com>
2025-02-07 20:57:52 +0200

    Added placeholder assets for symbols X and O.

--- gui/main.go
+++ gui/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"image/color"
+	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+const (
+	WindowW int = 640
+	WindowH int = 480
+)
+
+// Image assets
+var (
+	imageX *ebiten.Image
+	imageO *ebiten.Image
+)
+
+func init() {
+	var err error
+	imageX, _, err = ebitenutil.NewImageFromFile("assets/X.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	imageO, _, err = ebitenutil.NewImageFromFile("assets/O.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Game struct{}
 
@@ -14,16 +40,22 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.White)
+	screen.DrawImage(imageX, nil)
+
+	var v ebiten.DrawImageOptions
+	v.GeoM.Translate(64, 0)
+
+	screen.DrawImage(imageO, &v)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return WindowW, WindowH
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowSize(WindowW, WindowH)
+	ebiten.SetWindowTitle("TicTacGo")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
